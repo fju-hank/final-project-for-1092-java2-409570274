45b2f3db48f0b036a1b09e472c4a899edd8ebdd0
yu fang <cutebella529@gmail.com>
2021-06-24 13:25:24 +0800

    second code commit

--- src/main/java/com/DrinkShop.java
+++ src/main/java/com/DrinkShop.java
@@ -2,16 +2,18 @@ package com;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class DrinkShop {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
 
 
         JFrame frame = new JFrame("Drink Shop");
         frame.setLayout(null);
         frame.setBounds(800,600,800,600);
 
-        JButton button1 = new JButton("Bubble Milk  #60 ");
+        JButton button1 = new JButton("Bubble MilkTea  #60 ");
         button1.setBounds(100,100,200,50);
         button1.setHorizontalTextPosition(JButton.CENTER);
         frame.add(button1);
@@ -58,6 +60,178 @@ public class DrinkShop {
         list.setBounds(200,20,200,250);
         list.setLocation(450,100);
         frame.add(list);
+
+        Label label02 = new Label("Total:0#");
+        label02.setForeground(Color.RED);
+        label02.setBounds(200,500,100,20);
+        label02.setLocation(470,360);
+        frame.add(label02);
+
+        button1.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                String[] foods = list.getItems();
+                boolean items = false;
+                int index = 0;
+                for (int i = 0; i < foods.length; i++) {
+                    if (foods[i].contains("Bubble MilkTea")){
+                        index = i;
+                        items = true;
+                        break;
+                    }
+                }
+                if(items){
+                    String food = list.getItem(index);
+                    int x_index = food.indexOf("X");
+                    String value = food.substring(x_index + 1);
+                    food = food.substring(0, x_index + 1)
+                            + (Integer.parseInt(value) + 1);
+                    list.remove(index);
+                    list.add(food, index);
+                } else {
+                    list.add("Bubble MilkTea  X1");
+                }
+                String value = label02.getText();
+                int begin = value.indexOf(":");
+                int end = value.indexOf("#");
+                String money = value.substring(begin + 1, end);
+                String newValue = String.valueOf((Integer.parseInt(money) + 60));
+                value = value.replaceAll(money, newValue);
+                label02.setText(value);
+            }
+        });
+        button2.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                String[] foods = list.getItems();
+                boolean items = false;
+                int index = 0;
+                for (int i = 0; i < foods.length; i++) {
+                    if (foods[i].contains("Papaya Milk")){
+                        index = i;
+                        items = true;
+                        break;
+                    }
+                }
+                if(items){
+                    String food = list.getItem(index);
+                    int x_index = food.indexOf("X");
+                    String value = food.substring(x_index + 1);
+                    food = food.substring(0, x_index + 1)
+                            + (Integer.parseInt(value) + 1);
+                    list.remove(index);
+                    list.add(food, index);
+                } else {
+                    list.add("Papaya Milk  X1");
+                }
+                String value = label02.getText();
+                int begin = value.indexOf(":");
+                int end = value.indexOf("#");
+                String money = value.substring(begin + 1, end);
+                String newValue = String.valueOf((Integer.parseInt(money) + 50));
+                value = value.replaceAll(money, newValue);
+                label02.setText(value);
+            }
+        });
+        button3.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                String[] foods = list.getItems();
+                boolean items = false;
+                int index = 0;
+                for (int i = 0; i < foods.length; i++) {
+                    if (foods[i].contains("Black Tea")){
+                        index = i;
+                        items = true;
+                        break;
+                    }
+                }
+                if(items){
+                    String food = list.getItem(index);
+                    int x_index = food.indexOf("X");
+                    String value = food.substring(x_index + 1);
+                    food = food.substring(0, x_index + 1)
+                            + (Integer.parseInt(value) + 1);
+                    list.remove(index);
+                    list.add(food, index);
+                } else {
+                    list.add("Black Tea  X1");
+                }
+                String value = label02.getText();
+                int begin = value.indexOf(":");
+                int end = value.indexOf("#");
+                String money = value.substring(begin + 1, end);
+                String newValue = String.valueOf((Integer.parseInt(money) + 35));
+                value = value.replaceAll(money, newValue);
+                label02.setText(value);
+            }
+        });
+        button4.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                String[] foods = list.getItems();
+                boolean items = false;
+                int index = 0;
+                for (int i = 0; i < foods.length; i++) {
+                    if (foods[i].contains("Green Tea")){
+                        index = i;
+                        items = true;
+                        break;
+                    }
+                }
+                if(items){
+                    String food = list.getItem(index);
+                    int x_index = food.indexOf("X");
+                    String value = food.substring(x_index + 1);
+                    food = food.substring(0, x_index + 1)
+                            + (Integer.parseInt(value) + 1);
+                    list.remove(index);
+                    list.add(food, index);
+                } else {
+                    list.add("Green Tea  X1");
+                }
+                String value = label02.getText();
+                int begin = value.indexOf(":");
+                int end = value.indexOf("#");
+                String money = value.substring(begin + 1, end);
+                String newValue = String.valueOf((Integer.parseInt(money) + 35));
+                value = value.replaceAll(money, newValue);
+                label02.setText(value);
+            }
+        });
+        button5.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                String[] foods = list.getItems();
+                boolean items = false;
+                int index = 0;
+                for (int i = 0; i < foods.length; i++) {
+                    if (foods[i].contains("Secret Drink")){
+                        index = i;
+                        items = true;
+                        break;
+                    }
+                }
+                if(items){
+                    String food = list.getItem(index);
+                    int x_index = food.indexOf("X");
+                    String value = food.substring(x_index + 1);
+                    food = food.substring(0, x_index + 1)
+                            + (Integer.parseInt(value) + 1);
+                    list.remove(index);
+                    list.add(food, index);
+                } else {
+                    list.add("Secret Drink  X1");
+                }
+                String value = label02.getText();
+                int begin = value.indexOf(":");
+                int end = value.indexOf("#");
+                String money = value.substring(begin + 1, end);
+                String newValue = String.valueOf((Integer.parseInt(money) + 80));
+                value = value.replaceAll(money, newValue);
+                label02.setText(value);
+            }
+        });
 
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
