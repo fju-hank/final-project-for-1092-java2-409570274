68e0b15490403d58890858f0d8155dfd73079312
yu fang <cutebella529@gmail.com>
2021-06-26 20:04:38 +0800

    code commit

--- src/main/java/com/DrinkShop.java
+++ src/main/java/com/DrinkShop.java
@@ -6,7 +6,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class DrinkShop {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args){
 
 
         JFrame frame = new JFrame("Drink Shop");
@@ -16,31 +16,43 @@ public class DrinkShop {
         JButton button1 = new JButton("Bubble MilkTea  #60 ");
         button1.setBounds(100,100,200,50);
         button1.setHorizontalTextPosition(JButton.CENTER);
+        button1.setBackground(Color.ORANGE);
+        button1.setForeground(Color.blue);
         frame.add(button1);
 
         JButton button2 = new JButton("Papaya Milk  #50 ");
         button2.setBounds(100,150,200,50);
         button2.setHorizontalTextPosition(JButton.CENTER);
+        button2.setBackground(Color.ORANGE);
+        button2.setForeground(Color.blue);
         frame.add(button2);
 
         JButton button3 = new JButton("Black Tea  #35 ");
         button3.setBounds(100,200,200,50);
         button3.setHorizontalTextPosition(JButton.CENTER);
+        button3.setBackground(Color.ORANGE);
+        button3.setForeground(Color.blue);
         frame.add(button3);
 
         JButton button4 = new JButton("Green Tea  #35 ");
         button4.setBounds(100,250,200,50);
         button4.setHorizontalTextPosition(JButton.CENTER);
+        button4.setBackground(Color.ORANGE);
+        button4.setForeground(Color.blue);
         frame.add(button4);
 
         JButton button5 = new JButton("Secret Drink  #80 ");
         button5.setBounds(100,300,200,50);
         button5.setHorizontalTextPosition(JButton.CENTER);
+        button5.setBackground(Color.ORANGE);
+        button5.setForeground(Color.blue);
         frame.add(button5);
 
-        JButton button6 = new JButton("return");
+        JButton button6 = new JButton("RETURN");
         button6.setBounds(100,150,150,50);
-        button6.setLocation(300,400);
+        button6.setLocation(300,430);
+        button6.setBackground(Color.yellow);
+        button6.setForeground(Color.blue);
         frame.add(button6);
 
         JLabel label01 = new JLabel("Menu :");
@@ -56,7 +68,7 @@ public class DrinkShop {
         frame.add(label);
 
         List list = new List();
-        list.setForeground(Color.RED);
+        list.setForeground(Color.red);
         list.setBounds(200,20,200,250);
         list.setLocation(450,100);
         frame.add(list);
@@ -67,38 +79,35 @@ public class DrinkShop {
         label02.setLocation(470,360);
         frame.add(label02);
 
-        button1.addActionListener(new ActionListener(){
-            @Override
-            public void actionPerformed(ActionEvent e){
-                String[] foods = list.getItems();
-                boolean items = false;
-                int index = 0;
-                for (int i = 0; i < foods.length; i++) {
-                    if (foods[i].contains("Bubble MilkTea")){
-                        index = i;
-                        items = true;
-                        break;
-                    }
+        button1.addActionListener(e -> {
+            String[] foods = list.getItems();
+            boolean items = false;
+            int index = 0;
+            for (int i = 0; i < foods.length; i++) {
+                if (foods[i].contains("Bubble MilkTea")){
+                    index = i;
+                    items = true;
+                    break;
                 }
-                if(items){
-                    String food = list.getItem(index);
-                    int x_index = food.indexOf("X");
-                    String value = food.substring(x_index + 1);
-                    food = food.substring(0, x_index + 1)
-                            + (Integer.parseInt(value) + 1);
-                    list.remove(index);
-                    list.add(food, index);
-                } else {
-                    list.add("Bubble MilkTea  X1");
-                }
-                String value = label02.getText();
-                int begin = value.indexOf(":");
-                int end = value.indexOf("#");
-                String money = value.substring(begin + 1, end);
-                String newValue = String.valueOf((Integer.parseInt(money) + 60));
-                value = value.replaceAll(money, newValue);
-                label02.setText(value);
             }
+            if(items){
+                String food = list.getItem(index);
+                int x_index = food.indexOf("X");
+                String value = food.substring(x_index + 1);
+                food = food.substring(0, x_index + 1)
+                        + (Integer.parseInt(value) + 1);
+                list.remove(index);
+                list.add(food, index);
+            } else {
+                list.add("Bubble MilkTea  X1");
+            }
+            String value = label02.getText();
+            int begin = value.indexOf(":");
+            int end = value.indexOf("#");
+            String money = value.substring(begin + 1, end);
+            String newValue = String.valueOf((Integer.parseInt(money) + 60));
+            value = value.replaceAll(money, newValue);
+            label02.setText(value);
         });
         button2.addActionListener(new ActionListener(){
             @Override
@@ -228,6 +237,21 @@ public class DrinkShop {
                 int end = value.indexOf("#");
                 String money = value.substring(begin + 1, end);
                 String newValue = String.valueOf((Integer.parseInt(money) + 80));
+                value = value.replaceAll(money, newValue);
+                label02.setText(value);
+            }
+        });
+        button6.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+
+                list.removeAll();
+
+                String value = label02.getText();
+                int begin = value.indexOf(":");
+                int end = value.indexOf("#");
+                String money = value.substring(begin + 1, end);
+                String newValue = "0";
                 value = value.replaceAll(money, newValue);
                 label02.setText(value);
             }
